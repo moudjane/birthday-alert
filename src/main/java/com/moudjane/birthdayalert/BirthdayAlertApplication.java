dd19768cf51e6842260712da3cd0ca09770c8c98
moudjane <mathis.oudjane@gmail.com>
2024-10-19 19:46:29 +0200

    first commit

--- src/main/java/com/moudjane/birthdayalert/BirthdayAlertApplication.java
+++ src/main/java/com/moudjane/birthdayalert/BirthdayAlertApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BirthdayAlertApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BirthdayAlertApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdayAlertApplication.class, args);
+    }
 
 }

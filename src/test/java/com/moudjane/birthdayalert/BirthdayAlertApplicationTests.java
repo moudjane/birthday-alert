653b5ee53f8c6bbfeb0ebe39a0c561fd4540fbe8
moudjane <mathis.oudjane@gmail.com>
2024-10-19 19:43:45 +0200

    project initialization

---
+++ src/test/java/com/moudjane/birthdayalert/BirthdayAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moudjane.birthdayalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
